cc9cc2a62860659cb87794eacea38206786cac55
Lervinter <94516980+Lervinter@users.noreply.github.com>
2024-01-09 02:32:34 +0200

    Update Order.java

--- Step1/Order.java
+++ Step1/Order.java
@@ -6,9 +6,9 @@ public class Order {
     private Map<Product, Integer> orderDetails;
     private double totalPrice;
 
-    // Конструктори, гетери, сетери
+    
 
     public double calculateTotalPrice() {
-        // Розрахунок загальної вартості замовлення
+       
     }
 }

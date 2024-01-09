a4fbdf6f0805895a528a4a62f2736048520e2090
Lervinter <94516980+Lervinter@users.noreply.github.com>
2024-01-09 02:33:25 +0200

    Update ECommercePlatform.java

--- Step2/ECommercePlatform.java
+++ Step2/ECommercePlatform.java
@@ -5,27 +5,27 @@ public class ECommercePlatform {
     private Map<Integer, Product> products;
     private Map<Integer, Order> orders;
 
-    // Конструктор
+ 
 
     public void addUser(User user) {
-        // Додавання користувача
+       
     }
 
     public void addProduct(Product product) {
-        // Додавання товару
+       
     }
 
     public Order createOrder(int userId, Map<Product, Integer> orderDetails) {
-        // Створення замовлення
+        
     }
 
     public void updateStock(Product product, int quantity) {
-        // Оновлення запасів товару
+        
     }
 
     public void displayAvailableProducts() {
-        // Виведення списку доступних товарів
+        
     }
 
-    // Інші методи для роботи з користувачами, товарами та замовленнями
+   
 }
